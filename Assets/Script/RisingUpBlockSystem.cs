99737c59c4148d2bad8cb034e7f345f997e8bdc2
nature_win <napnature8436@gmail.com>
2026-06-26 01:14:16 +0900

    add: rising up block

---
+++ Assets/Script/RisingUpBlockSystem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RisingUpBlockSystem : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isRise = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isRise)
+        {
+            RisingUp();
+            isRise = true;
+        }
+    }
+    private void RisingUp()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.gravityScale = -1.5f;
+    }
+}
